cf504bb608b964dc5152cbf96875ca288d58ce78
Bjourdain <jourdain@lacroixrouge-brest.fr>
2020-09-01 12:45:24 +0200

    typage

---
+++ Sequence1_recursivite/typage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 17 14:29:04 2020
+
+@author: bruno
+"""
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+somme=add('b','a')
+print(somme)
